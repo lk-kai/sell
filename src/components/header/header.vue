e8b055ceb75069d8cebd2b2e1f5b8634171da795
lk-kai <806306842@qq.com>
2020-02-17 00:49:34 +0800

    star 组件

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,13 +17,34 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div class="support-count" v-if="seller.supports" @click="showDetail">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper" @click="showDetail">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img width="100%" height="100%" :src="seller.avatar" alt="">
+    </div>
+    <div class="detail" v-show="detailShow">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+          <star :size="48" :score="2.7"></star>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import star from '../star/star'
 export default {
   props: {
     seller: {
@@ -36,8 +57,17 @@ export default {
       console.log(this.seller)
     }, 20)
   },
+  components: {
+    star
+  },
   data () {
     return {
+      detailShow: false
+    }
+  },
+  methods: {
+    showDetail () {
+      this.detailShow = true
     }
   }
 }
@@ -45,9 +75,13 @@ export default {
 <style lang="stylus">
 @import '../../common/stylus/mixin'
   .header
+    position relative
     color #ffffff
-    background #000000
+    // background #999
+    overflow hidden
+    background rgba(7, 17, 27, 0.5)
     .content-wrapper
+      position relative
       padding 24px 12px 18px 24px
       font-size 0
       .avatar
@@ -98,6 +132,83 @@ export default {
             &.invoice
               bg-image('invoice_1')
           .text
-            font-size 12px
+            font-size 10px
             line-height 12px
+      .support-count
+        position absolute
+        right 12px
+        bottom 14px
+        padding 0 8px
+        height 24px
+        line-height 24px
+        border-radius 14px
+        background rgba(0, 0, 0, 0.2)
+        text-align center
+        .count
+          font-size 10px
+          vertical-align top
+        .icon-keyboard_arrow_right
+          margin-left 2px
+          line-height 24px
+          font-size 10px
+    .bulletin-wrapper
+      position relative
+      height 28px
+      line-height 28px
+      padding 0 22px 0 12px
+      white-space nowrap
+      overflow hidden
+      text-overflow ellipsis
+      background rgba(7, 17, 27, 0.2)
+      .bulletin-title
+        display inline-block
+        vertical-align top
+        margin-top 8px
+        width 22px
+        height 12px
+        bg-image('bulletin')
+        background-size 22px 12px
+        background-repeat no-repeat
+      .bulletin-text
+        vertical-align top
+        margin 0 4px
+        font-size 10px
+      .icon-keyboard_arrow_right
+        position absolute
+        font-size 10px
+        right 12px
+        top 8px
+    .background
+      position absolute
+      top 0
+      left 0
+      width 100%
+      height 100%
+      z-index -1
+      filter blur(10px)
+    .detail
+      position fixed
+      top 0
+      left 0
+      z-index 100
+      width 100%
+      height 100%
+      overflow auto
+      background rgba(7, 17, 27, 0.8)
+      .detail-wrapper
+        min-height 100%
+        width 100%
+        .detail-main
+          margin-top 64px
+          padding-bottom 64px
+          .name
+            line-height 16px
+            text-align center
+            font-size 16px
+      .detail-close
+        position relative
+        width 32px
+        height 32px
+        margin -64px auto 0 auto
+        clear both
 </style>
