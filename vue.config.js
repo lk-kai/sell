d1420826cb4cfa3b8e7631cf1234124e8601a605
lk-kai <806306842@qq.com>
2020-04-01 22:10:52 +0800

    cube-ui

--- vue.config.js
+++ vue.config.js
@@ -9,19 +9,37 @@ var apiRoutes = express.Router()
 app.use('/api', apiRoutes)
 
 module.exports = {
-    // baseUrl:
-    devServer: {
-        before: function (app) {
-            //可请求   api/data
-            app.get('/api/seller', function (req, res) {
-              res.json({code: 0, data: seller})
-            }),
-            app.get('/api/goods', function (req, res) {
-              res.json({code: 0, data: goods})
-            }),
-            app.get('/api/ratings', function (req, res) {
-              res.json({code: 0, data: ratings})
-        })
-        }
+  // baseUrl:
+  devServer: {
+      before: function (app) {
+          //可请求   api/data
+          app.get('/api/seller', function (req, res) {
+            res.json({code: 0, data: seller})
+          }),
+          app.get('/api/goods', function (req, res) {
+            res.json({code: 0, data: goods})
+          }),
+          app.get('/api/ratings', function (req, res) {
+            res.json({code: 0, data: ratings})
+      })
+      }
+  },
+
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        'import': [
+          './src/theme'
+        ]
+      }
     }
+  },
+
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true
+    }
+  }
 }
