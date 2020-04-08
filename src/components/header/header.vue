cacee0a84e84da412869eeeb2ca66a7a8f356a24
lk-kai <806306842@qq.com>
2020-04-09 00:57:12 +0800

    商家信息

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -28,22 +28,46 @@
     <div class="background">
       <img :src="seller.avatar" alt height="100%" width="100%" />
     </div>
-    <div class="detail" v-show="detailShow">
-      <div class="detail-wrapper clearfix">
-        <div class="detail-main">
-          <h1 class="name">{{seller.name}}</h1>
-          <star :score="2.7" :size="48"></star>
+    <transition name="fade">
+      <div class="detail" v-show="detailShow">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="start-wrapper">
+              <star :score="2.7" :size="48"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="(item, index) in seller.supports" :key="index">
+                <supporticon :size="2" :type="item.type" class="icon"></supporticon>
+                <span class="text">{{item.description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
+          </div>
+        </div>
+        <div @click="closeDetail" class="detail-close">
+          <i class="icon-close"></i>
         </div>
       </div>
-      <div @click="closeDetail" class="detail-close">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
 <script>
 import star from '../star/star'
+import supporticon from '../support-ico/support-ico'
 export default {
   props: {
     seller: {
@@ -54,7 +78,8 @@ export default {
     this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
   },
   components: {
-    star
+    star,
+    supporticon
   },
   data() {
     return {
@@ -73,6 +98,12 @@ export default {
 </script>
 <style lang="stylus" scoped>
 @import '../../common/stylus/mixin'
+.fade-enter-active, .fade-leave-active
+  transition: all 0.5s
+.fade-enter-to, .fade-leave
+  opacity: 1
+.fade-enter, .fade-leave-to
+  opacity: 0
 .header
   position: relative
   color: #ffffff
@@ -193,6 +224,7 @@ export default {
     height: 100%
     overflow: auto
     background: rgba(7, 17, 27, 0.8)
+    backdrop-filter: blur(8px)
     .detail-wrapper
       min-height: 100%
       width: 100%
@@ -203,6 +235,46 @@ export default {
           line-height: 16px
           text-align: center
           font-size: 16px
+        .start-wrapper
+          margin-top: 18px
+          padding: 2px 0
+          text-align: center
+        .title
+          display: flex
+          width: 80%
+          margin: 28px auto 24px auto
+          .line
+            flex: 1
+            position: relative
+            top: -6px
+            border-bottom: 1px solid rgba(255, 255, 255, 0.2)
+          .text
+            padding: 0 12px
+            font-weight: 700
+            font-size: 14px
+        .supports
+          width: 80%
+          margin: 0 auto
+          .support-item
+            padding: 0 12px
+            margin-bottom: 12px
+            &:last-child
+              margin-bottom: 0
+            .icon
+              display: inline-block
+              vertical-align: top
+              margin-right: 6px
+            .text
+              vertical-align: top
+              line-height: 12px
+              font-size: 12px
+        .bulletin
+          width: 80%
+          margin: 0 auto
+          .content
+            padding: 0 12px
+            line-height: 24px
+            font-size: 12px
     .detail-close
       position: relative
       width: 32px
