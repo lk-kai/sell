c103ba18b555b110ab2a41ee2b394956c44d90dd
lk-kai <806306842@qq.com>
2020-04-13 21:04:30 +0800

    add baseUrl

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -1,10 +1,14 @@
 import axios from 'ts-axios-new'
 const ERR_OK = 0
-
+const urlMap = {
+  development: '/',
+  production: '47.114.163.129/sell/'
+}
+const baseUrl = urlMap[process.env.NODE_ENV]
 export function get(url) {
   return function (params = {}) {
-    // return axios.get(baseUrl + url, {
-    return axios.get(url, {
+    return axios.get(baseUrl + url, {
+      // return axios.get(url, {
       params
     }).then((res) => {
       const { code, data } = res.data
